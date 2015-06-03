ad36ff314cbd0198aa8882f46e61aa189efd0b33
almathie <alexandremathieu@Alexandres-MacBook-Pro.local>
2015-06-03 22:28:13 +0200

    Add Net:HTTP details

--- lib/patches/net_patches.rb
+++ lib/patches/net_patches.rb
@@ -9,6 +9,21 @@ if (defined?(Net) && defined?(Net::HTTP))
     end
     alias request_without_mini_profiler request
     alias request request_with_mini_profiler
-  end
 
+    def connect_with_mini_profiler(*args, &block)
+      Rack::MiniProfiler.step("Opening Connection") do
+        connect_without_mini_profiler(*args, &block)
+      end
+    end
+    alias connect_without_mini_profiler connect
+    alias connect connect_with_mini_profiler
+
+    def transport_request_with_mini_profiler(*args, &block)
+      Rack::MiniProfiler.step("Transporting Data") do
+        transport_request_without_mini_profiler(*args, &block)
+      end
+    end
+    alias transport_request_without_mini_profiler transport_request
+    alias transport_request transport_request_with_mini_profiler
+  end
 end
